23fdcab1ecafb63640bb8723ddcbbcf5d2d5a008
Sumeet Singh Mankoo <sumeet.mankoo@gmail.com>
2019-07-02 22:18:25 -0400

    added local settings for transmission

--- install-transmission.sh
+++ install-transmission.sh
@@ -11,6 +11,7 @@ fi
 
 if [ -f "${SETTINGS_FILE}" ]; then
     cat "${SETTINGS_FILE}" | sudo tee /etc/transmission-daemon/settings.json > /dev/null 2>&1
+    cp -f "${SETTINGS_FILE}" /home/viking/.config/transmission-daemon/settings.json
     echo "Standard Trnsmission settings applied."
 else
     echo "Missing settings.json. Unable to apply standard Transmission settings."
@@ -24,5 +25,12 @@ echo "[Service]
 User=viking" | sudo tee /etc/systemd/system/transmission-daemon.service.d/run-as-user.conf > /dev/null 2>&1
 sudo systemctl daemon-reload
 sudo service transmission-daemon start
-echo "Transmission daemon started"
 
+daemon_status=`systemctl is-active transmission-daemon`
+
+daemon_status=`systemctl is-active transmission-daemon`
+if [ "${daemon_status}" = "active" ]; then
+    echo "Transmission daemon started"
+else
+    echo "Failed to start transmission daemon"
+fi
---
+++ setup-plex.sh
@@ -0,0 +1,6 @@
+sudo apt install curl -y
+echo deb https://downloads.plex.tv/repo/deb public main | sudo tee /etc/apt/sources.list.d/plexmediaserver.list
+curl https://downloads.plex.tv/plex-keys/PlexSign.key | sudo apt-key add -
+
+sudo apt install plexmediaserver -y
+
