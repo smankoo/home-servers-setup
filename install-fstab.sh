54edede7e8c9b7fae38e065fdbd903f3182d8dbd
Sumeet <sumeet.mankoo@gmail.com>
2019-07-27 23:55:14 -0400

    now preserves the original fstab

--- install-fstab.sh
+++ install-fstab.sh
@@ -1,12 +1,18 @@
 sudo apt install nfs-common -y
-dt=`date '+(%d-%m-%Y_%H-%M-%S)'`
+dt=`date '+%d-%m-%Y_%H-%M-%S'`
 
 # read -p "Enter the password for file shares: " CIFS_PASS
 
 FSTAB_SKEL="skel/fstab"
 
+if [ -f /etc/fstab.orig ]; then
+    cp /etc/fstab /etc/fstab.orig
+fi
+
 sudo cp /etc/fstab /etc/fstab.${dt}
-true | sudo tee /etc/fstab > /dev/null 2>&1
+# true | sudo tee /etc/fstab > /dev/null 2>&1
+
+sudo cp /etc/fstab.orig /etc/fstab
 
 grep . "${FSTAB_SKEL}" | grep -v "#" | while read row
 do
--- install-sickrage.sh
+++ install-sickrage.sh
@@ -10,7 +10,7 @@ fi
 
 git clone https://github.com/sickrage/sickrage.git .sickrage
 
-pip3 install -r .sickrage/requirements.txt
+pip3 install -r ~/.sickrage/requirements.txt
 
 if [ -f /etc/init.d/sickrage ]; then
     sudo rm /etc/init.d/sickrage
@@ -24,7 +24,7 @@ sudo update-rc.d -f sickrage remove
 
 # There seems to be a bug. Python version specified in /etc/default/sickrage is not used
 # while starting the service. So the python version needs to be "hardcoded"
-sed -i.bak 's/python2.7/python3.7/' ~/.sickrage/runscripts/init.ubuntu
+sed -i.bak 's/python2.7/python3/' ~/.sickrage/runscripts/init.ubuntu
 
 sudo cp ~/.sickrage/runscripts/init.ubuntu /etc/init.d/sickrage
 sudo chmod +x /etc/init.d/sickrage
@@ -32,7 +32,7 @@ sudo chmod +x /etc/init.d/sickrage
 echo "SR_HOME=/home/${USER}/.sickrage/
 SR_DATA=/home/${USER}/.sickrage/
 SR_USER=${USER}
-PYTHON_BIN=/usr/bin/python3.7" | sudo tee /etc/default/sickrage > /dev/null 2>&1
+PYTHON_BIN=/usr/bin/python3" | sudo tee /etc/default/sickrage > /dev/null 2>&1
 
 sudo chmod +x /etc/default/sickrage
 
