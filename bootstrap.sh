5ea1d434021f645b1f7e35a2e4c944509a322d06
Sumeet Singh Mankoo <sumeet.mankoo@gmail.com>
2019-07-29 20:15:32 -0400

    added tv2 to fstab

---
+++ bootstrap.sh
@@ -0,0 +1,6 @@
+sudo apt install git -y
+rm -rf ~/home-servers-setup
+git clone https://github.com/smankoo/home-servers-setup.git && \
+cd home-servers-setup && \
+chmod 755 *.sh &&
+./install-fstab.sh
--- install-plex.sh
+++ install-plex.sh
@@ -1,6 +1,7 @@
-sudo apt-get update && sudo apt-get upgrade -y
-sudo apt-get install curl gnupg -y
+sudo apt-get update
+sudo apt-get install curl -y
+
 echo deb https://downloads.plex.tv/repo/deb public main | sudo tee /etc/apt/sources.list.d/plexmediaserver.list
 curl https://downloads.plex.tv/plex-keys/PlexSign.key | sudo apt-key add -
 sudo apt-get update
-sudo apt install plexmediaserver -y
+sudo apt-get install plexmediaserver -y
